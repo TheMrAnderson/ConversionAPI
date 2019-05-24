c056a677c4e5572297cd66ce8ab03489fc67e638
Matt Anderson <mpanderson06@gmail.com>
2019-05-24 17:23:30 -0500

    Making changes to run a production web server

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from apiv1 import blueprint as api1
 
 app = Flask(__name__)
 app.register_blueprint(api1)
-app.run(debug=True, port=80, host="0.0.0.0")
+# app.run(debug=True, port=80, host="0.0.0.0")
