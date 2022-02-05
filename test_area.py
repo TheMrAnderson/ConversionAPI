622657109d1e08cf86c987407302008428bfb665
Matt Anderson <mpanderson06@fastmail.com>
2022-02-04 22:55:38 -0600

    Formatting changes

--- test_area.py
+++ test_area.py
@@ -1,46 +1,47 @@
 import unittest
 from Conversions import area
 
+
 class Tests(unittest.TestCase):
 
-	def test_acre_to_ft2(self):
-		self.assertEqual(area.acre_to_ft2(1), 43560)
+    def test_acre_to_ft2(self):
+        self.assertEqual(area.acre_to_ft2(1), 43560)
 
-	def test_acre_to_mile2(self):
-		self.assertEqual(area.acre_to_mile2(1), 0.0015625)
+    def test_acre_to_mile2(self):
+        self.assertEqual(area.acre_to_mile2(1), 0.0015625)
 
-	def test_acre_to_yard2(self):
-		self.assertEqual(area.acre_to_yard2(1), 4840)
+    def test_acre_to_yard2(self):
+        self.assertEqual(area.acre_to_yard2(1), 4840)
 
-	def test_ft2_to_acre(self):
-		self.assertEqual(area.ft2_to_acre(43560), 1)
+    def test_ft2_to_acre(self):
+        self.assertEqual(area.ft2_to_acre(43560), 1)
 
-	def test_ft2_to_in2(self):
-		self.assertEqual(area.ft2_to_in2(1), 144)
+    def test_ft2_to_in2(self):
+        self.assertEqual(area.ft2_to_in2(1), 144)
 
-	def test_in2_to_ft2(self):
-		self.assertEqual(area.in2_to_ft2(144), 1)
+    def test_in2_to_ft2(self):
+        self.assertEqual(area.in2_to_ft2(144), 1)
 
-	def test_in2_to_yard2(self):
-		self.assertEqual(area.in2_to_yard2(1296), 1)
+    def test_in2_to_yard2(self):
+        self.assertEqual(area.in2_to_yard2(1296), 1)
 
-	def test_mile2_to_acre(self):
-		self.assertEqual(area.mile2_to_acre(1), 640)
+    def test_mile2_to_acre(self):
+        self.assertEqual(area.mile2_to_acre(1), 640)
 
-	def test_rod2_to_yard2(self):
-		self.assertEqual(area.rod2_to_yard2(1), 30.25)
+    def test_rod2_to_yard2(self):
+        self.assertEqual(area.rod2_to_yard2(1), 30.25)
 
-	def test_yard2_to_acre(self):
-		self.assertEqual(area.yard2_to_acre(4840), 1)
+    def test_yard2_to_acre(self):
+        self.assertEqual(area.yard2_to_acre(4840), 1)
 
-	def test_yard2_to_in2(self):
-		self.assertEqual(area.yard2_to_in2(1), 1296)
+    def test_yard2_to_in2(self):
+        self.assertEqual(area.yard2_to_in2(1), 1296)
 
-	def test_yard2_to_rod2(self):
-		self.assertEqual(area.yard2_to_rod2(30), 0.991735537190082)
+    def test_yard2_to_rod2(self):
+        self.assertEqual(area.yard2_to_rod2(30), 0.991735537190082)
 
-	def test_circumference(self):
-		self.assertEqual(area.circumference(50), 157.0796326795)
+    def test_circumference(self):
+        self.assertEqual(area.circumference(50), 157.0796326795)
 
 
 if __name__ == '__main__':
